ca0d80537265931a11678bb19ab8816fcb73e55e
Kev Comedia <kevcomedia@gmail.com>
2018-03-07 12:03:05 +0800

    Feat: hash password on pre-save hook
    I made a sample save and the password is indeed hashed. Tests are of
    course preferable, but I've read that testing a pre-save hook requires a
    running database.

--- models/user.js
+++ models/user.js
@@ -1,3 +1,5 @@
+const async = require('async')
+const bcrypt = require('bcrypt-nodejs')
 const mongoose = require('mongoose')
 
 const { Schema } = mongoose
@@ -14,6 +16,26 @@ const UserSchema = new Schema({
     _id: { type: Schema.Types.ObjectId, ref: 'Interest' },
     wantsToBeLeader: Boolean,
   }],
+})
+
+UserSchema.pre('save', function hashPassword(next) {
+  async.waterfall(
+    [
+      (callback) => {
+        bcrypt.genSalt(10, callback)
+      },
+      (salt, callback) => {
+        // eslint-disable-next-line no-invalid-this
+        bcrypt.hash(this.password, salt, null, callback)
+      },
+      (hash, callback) => {
+        // eslint-disable-next-line no-invalid-this
+        this.password = hash
+        callback()
+      },
+    ],
+    next,
+  )
 })
 
 const UserModel = mongoose.model('User', UserSchema)
