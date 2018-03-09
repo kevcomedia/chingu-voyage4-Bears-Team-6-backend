7b490d2d5ef44d202f591a5294136a0f0c1c814d
Vladimir Logachev <vlogachev@allmax.team>
2018-03-09 18:06:37 +0700

    chore: postinstall script! :boom:

--- app.js
+++ app.js
@@ -9,10 +9,10 @@ const app = express()
 app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'node_modules', 'wayt-frontend', 'build', 'index.html'))
+  res.sendFile(path.resolve(__dirname, 'static', 'index.html'))
 })
 
-app.use('/static', express.static(path.resolve(__dirname, 'node_modules', 'wayt-frontend', 'build')))
+app.use('/static', express.static(path.resolve(__dirname, 'static')))
 
 app.use('/', routes)
 
---
+++ install-frontend.js
@@ -0,0 +1,50 @@
+/* eslint-disable no-console */
+
+const { execSync } = require('child_process')
+
+const frontendGitUrl = 'https://github.com/chingu-voyage4/Bears-Team-6.git'
+const dirName = 'Bears-Team-6'
+const branchName = 'current-sprint'
+
+const print = (str) => console.log(`${str}\n`)
+const printInfo = (str) => print(`   ... ${str}`)
+const execAndPrint = (command) => print(execSync(command))
+
+// May not work on Windows OS. should be fixed
+
+const installFrontendScript = () => {
+  printInfo('Removing previous frontend repo')
+  execAndPrint('rm -rf ./Bears-Team-6')
+
+  printInfo('Removing previous frontend build')
+  execAndPrint('rm -rf ./static')
+
+  printInfo('Cloning fresh frontend repo')
+  execAndPrint(`git clone -b ${branchName} ${frontendGitUrl}`)
+
+  printInfo('Installing frontend')
+  execAndPrint(`cd ${dirName} && npm i && cd ..`)
+
+  printInfo('Testing frontend')
+  execAndPrint(`cd ${dirName} && npm test && cd ..`)
+
+  printInfo('Building frontend')
+  execAndPrint(`cd ${dirName} && npm run build && cd ..`)
+
+  printInfo('Building frontend')
+  execAndPrint(`cd ${dirName} && npm run build && cd ..`)
+
+  printInfo('Moving frontend')
+  execAndPrint(`mv ./${dirName}/build ./static`)
+
+  printInfo('Removing frontend repo')
+  execAndPrint('rm -rf ./Bears-Team-6')
+}
+
+try {
+  installFrontendScript()
+  print('SUCCESS!')
+} catch (e) {
+  print(`ERROR! \n${e}`)
+}
+
