eda62d1821512cc1ec05d49dcfa4bcb067b76e4a
Tung Teng <tungnt_580@vnu.edu.vn>
2018-03-09 22:41:44 +0700

    Serve all requests that not to be handled before to index.html

--- app.js
+++ app.js
@@ -8,12 +8,12 @@ const app = express()
 
 app.use(bodyParser.json())
 
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'static', 'index.html'))
-})
-
 app.use('/static', express.static(path.resolve(__dirname, 'static')))
 
 app.use('/', routes)
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'static', 'index.html'))
+})
 
 module.exports = app
